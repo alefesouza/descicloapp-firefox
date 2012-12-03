1f30ecb5472466a9bd3034f16959ed0b4a11e90e
alefesouza <alefe.mtv@gmail.com>
2012-12-02 22:38:13 -0200

    Codigo da versao 4.3.1

---
+++ DescicloApp/chrome/button2.js
@@ -0,0 +1,26 @@
+function installButton()
+{
+var id = "descicloapp-button-1";
+var toolbarId = "nav-bar";
+ 
+var toolbar = document.getElementById(toolbarId);
+ 
+toolbar.insertItem(id, toolbar.lastChild);
+toolbar.setAttribute("currentset", toolbar.currentSet);
+document.persist(toolbar.id, "currentset");
+ 
+toolbar.collapsed = false;
+}
+ 
+function firstRun(extensions)
+{
+    var extension = extensions.get("descicloapp@aloogle");
+ 
+    if (extension.firstRun)
+     installButton();
+}
+ 
+if (Application.extensions)
+    firstRun(Application.extensions);
+else
+    Application.getExtensions(firstRun);
